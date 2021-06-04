7a1b0ae0a3391eed738938e94bc828da4fa4c8ca
caiomcg <caiomcg@gmail.com>
2021-06-04 11:44:59 -0300

    test: add helpers tests

---
+++ src/helpers/error_test.go
@@ -0,0 +1,16 @@
+package helpers
+
+import "testing"
+import "net/http"
+
+func TestGetErrorFromHTTPError(t *testing.T) {
+    err := NewHTTPError(http.StatusOK, "key", "msg")
+    expected := "key: msg"
+
+    if err.Error() != expected {
+        t.Errorf(
+            "Should return a message with the following pattern: %s",
+            expected,
+        )
+    }
+}
